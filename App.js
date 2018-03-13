154a240c875e7bf178b2f39d8da947760360475b
Steveraaen <sraaen@gmail.com>
2018-03-13 13:35:02 -0400

    displaying content

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ console.disableYellowBox = true;
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  AppState,
   Dimensions,
   FlatList,
   StyleSheet,
@@ -13,50 +14,73 @@ import {
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Details from './Details.js'
-import get from './helpers'
+import getAll from './helpers'
 
 export default class HomeScreen extends Component {
     static navigationOptions = {
-    title: 'NYC Location Analysis',
+      header: null
   };
   constructor(props) {
     super(props);
     this.state={
       input: "Madison Square Garden"
     }  
-    this.getData = this.getData.bind(this)
-/*    this.handlePress = this.handlePress.bind(this)*/
+    /*this.getData = this.getData.bind(this)*/
+/*    this._handleAppStateChange = this._handleAppStateChange.bind(this)
+    this.handlePress = this.handlePress.bind(this)
     }
-  getData() {
-
-
+      _handleAppStateChange = (nextAppState) => {
+    if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
+    }
+    this.setState({
+      appState: nextAppState,
+    });*/
   }
- componentWillMount() {
-      get().then((response) => {
-      console.log(response)
-      this.setState({
-        aspSigns: response[0],
-        busStops: response[1],
-        parkingMeters: response[2],
-      })
-    })
+
+ componentDidMount() {
+  /*     AppState.addEventListener('change', this._handleAppStateChange);*/
+      navigator.geolocation.getCurrentPosition(function(pos) {
+            var { longitude, latitude, accuracy, heading } = pos.coords
+            this.setState({
+                uLongitude: pos.coords.longitude,
+                uLatitude: pos.coords.latitude,
+                uLnglat: [pos.coords.longitude, pos.coords.latitude]
+            }, () => {
+             getAll(this.state.longitude, this.state.latitude).then((response) => {      
+              this.setState({
+                citibikeStops: response[0],
+                subwayStops: response[1],
+                parkingASP: response[2],
+                busStops: response[3],
+                parkingMeters: response[4]
+              })
+            })
+            })   
+
+        }.bind(this))    
+console.log(this.state)
+
+ }
+ handlePress() {
+  console.log(this.state.uLongitude)
+  getAll(this.state.uLongitude, this.state.uLatitude)
  }
   render() {
     const { navigate } = this.props.navigation;
-
+    if(this.state.citibikeStops && this.state.busStops & this.state.subwayStops, this.state.parkingASP) {
     return (
       <View style={styles.container}>
-        <View style={styles.greeting}>
+{/*        <View style={styles.greeting}>
         <TouchableOpacity
           onPress={() => navigate('Details')}
          
           title= "Details" >
           <Text style={{fontSize: 24, color: 'white'}}>
-            hello
+            NYC Transport Options
           </Text>
         </TouchableOpacity>
-        </View>  
-        <View style={styles.searchBar}> 
+        </View>  */}
+        <View > 
           <View style = {styles.searchBarInput}>
             <TextInput 
               style={{textAlign: 'center', fontSize: 20, color: 'black', backgroundColor: 'white'}}
@@ -64,15 +88,52 @@ export default class HomeScreen extends Component {
             />
         </View>
         </View>
-          <View style={styles.resultsContainer}> 
+          <View style={styles.resultsContainer}>
+            <TouchableOpacity onPress={() => this.handlePress()} > 
+              <View style={styles.card}>
+             
+                <Text style={styles.cardTitle}>Subways</Text>
+                <Text style={styles.cardText}>Subway Stations  <Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>{this.state.subwayStops.length}</Text></Text>
+                <Text style={styles.cardText}>Nearest Station  <Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>{this.state.subwayStops[0].properties.stop_name}</Text></Text>
+                <Text style={styles.cardText}>Distance  <Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>{this.state.subwayStops[0].distance.dist.toFixed(2)} miles</Text></Text>
+              </View>
+            
+            </TouchableOpacity> 
+            <TouchableOpacity  > 
+              <View style={styles.card}>
+                <Text style={styles.cardTitle}>Citibike</Text>
+                <Text style={styles.cardText}>Bike Stations<Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>   {this.state.citibikeStops.length}</Text></Text>
+                <Text style={styles.cardText}>Nearest Station  <Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>  {this.state.citibikeStops[0].properties.stationName}</Text></Text>
+                <Text style={styles.cardText}>Distance  <Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>   {this.state.citibikeStops[0].distance.dist.toFixed(2)} miles</Text></Text>
+                <Text style={styles.cardText}>Available Bikes<Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>   {this.state.citibikeStops[0].properties.availableBikes}</Text></Text>
+              </View>
+            </TouchableOpacity>             
+            <TouchableOpacity  > 
+              <View style={styles.card}>
+                <Text style={styles.cardTitle}>Busses</Text>
+                <Text style={styles.cardText}>Bus Stops<Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>{this.state.busStops.length}</Text></Text>
+                <Text style={styles.cardText}>Nearest Stop  <Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>{this.state.busStops[0].name}</Text></Text>
+                <Text style={styles.cardText}>Route Name<Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>{this.state.busStops[0].routes[0].longName}</Text></Text>
 
-              <View style={styles.card}><Text style={{fontSize: 24, color: 'white', textAlign: 'center'}}></Text></View>
-              <View style={styles.card}><Text>Box 1</Text></View>
-              <View style={styles.card}><Text>Box 1</Text></View>
-              <View style={styles.card}><Text>Box 1</Text></View>         
+              </View>
+            </TouchableOpacity> 
+            <TouchableOpacity> 
+              <View style={styles.card}>                
+                <Text style={styles.cardTitle}>Street Parking</Text>
+                <Text style={styles.cardText}>ASP Spaces (Approx)<Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>   {this.state.parkingASP.length*4}</Text></Text>
+                <Text style={styles.cardText}>ASP Nearest  <Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>{this.state.parkingASP[0].properties.T}</Text></Text>
+                <Text style={styles.cardText}>Meters  <Text style={{textAlign: 'right', color: 'white', fontWeight: 'bold'}}>{this.state.parkingMeters.length}</Text></Text>
+
+              </View> 
+            </TouchableOpacity>   
+      
           </View>
       </View>
     ) 
+  } else {
+    return (
+      <View><Text>Waiting ...</Text></View>
+      )}
   }
 }
 const width = Dimensions.get('window').width
@@ -87,48 +148,64 @@ const styles = StyleSheet.create({
   greeting: {
     marginTop: 8,
     marginBottom: 4,
-    flex: .08,
+    flex: .06,
     backgroundColor:'gray',
     justifyContent: 'center',
     alignItems: 'center'
   },
 // --------------------- searchbar parent
     searchBar: {
-    marginTop: 12,
-    flex: .1,
+
+    flex: .08,
     justifyContent: 'center',
     backgroundColor:'gray',
   },
     searchBarInput: {
-     height: 36,
+     height: 32,
      paddingTop:4,
+     marginTop: 24,
      paddingBottom:4,
      backgroundColor: 'white'
     },
 // -------------------------results parent
     resultsContainer: {
-      marginTop:12,
-      paddingTop:22,
+ /*     marginTop:12,
+      paddingTop:12,*/
       flex: .8,
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-      alignItems: 'stretch',
-      backgroundColor: 'coral'
+      flexDirection: 'column',
+    /*  flexWrap: 'wrap',*/
+      backgroundColor: 'black'
     },
     card: {
-      backgroundColor: 'black',
-      height: 150,
-      width: 150,
-      margin: 10,
+      backgroundColor:'#03003F',
+      height: 130,
+ /*     width: 150,*/
+      margin: 4,
       alignItems: 'center',
       justifyContent: 'center'
+    },
+    cardTitle: {
+      fontSize: 18, 
+      fontWeight: 'bold',
+      color: 'white', 
+      textAlign: 'center',
+      paddingBottom: 8
+    },
+    cardText: {
+      fontSize: 14, 
+      fontWeight: 'bold',
+      color: 'yellow', 
+      paddingBottom: 8
+    },
+    innerCard: {
+      alignItems: 'flex-start'
     }
 
 });
 const nyclocalytics = StackNavigator({
   Home: { screen: HomeScreen },
   Details: { screen: Details },
+  headerMode: 'screen' 
 });
 
 AppRegistry.registerComponent('nyclocalytics', () => nyclocalytics);
--- helpers.js
+++ helpers.js
@@ -1,20 +1,41 @@
 var axios = require ('axios')
 
-module.exports = async function() {
+module.exports = async function getAll(lng, lat) {
 	try {
+
+	 const citibikePromise = axios('https://subs-backend.herokuapp.com/api/bikes/', {
+            params: {
+                lng: -73.9839440,
+                lat: 40.6766600,                 
+/*                lng: parseFloat(lng).toFixed(6),
+                lat: parseFloat(lat).toFixed(6) */              
+            }
+        })
 	 const subwayPromise = axios('https://subs-backend.herokuapp.com/api/stops/', {
             params: {
                 lng: -73.9839440,
                 lat: 40.6766600,               
             }
         })
-    const bussesPromise = axios('https://bustime.mta.info/api/where/stops-for-location.json?lat=40.6766600&lon=-73.9839440&latSpan=0.01&lonSpan=0.01&key=d780a773-f505-402c-b6cb-4a9939fbe07a')
-    const metersPromise =  axios('http://127.0.0.1:5000/api/meters?lat=40.6766600&lon=-73.9839440')
+	 const aspPromise = axios.get('https://streetparker.herokuapp.com/mon', {
+            params: { coordinates: [-73.9839440, 40.6766600]                            
+            }
+         })
+    const bussesPromise = axios('https://bustime.mta.info/api/where/stops-for-location.json?latSpan=0.01&lonSpan=0.01&key=d780a773-f505-402c-b6cb-4a9939fbe07a', {
+            params: {
+                lon: -73.9839440,
+                lat: 40.6766600,             
+            }    	
+    })    
+    const metersPromise =  axios('https://streetparker.herokuapp.com/api/meters', {
+            params: { coordinates: [-73.9839440, 40.6766600]                            
+            }  	    	
+    })
 
-    const [subwayStops, busStops, parkingMeters] = await Promise.all([subwayPromise, bussesPromise, metersPromise]);
-    return [subwayStops.data, busStops.data.data.stops, parkingMeters.data]
+    const [citibikeStops, subwayStops, aspSigns, busStops, parkingMeters] = await Promise.all([citibikePromise, subwayPromise, aspPromise, bussesPromise, metersPromise]);
+    return [citibikeStops.data, subwayStops.data, aspSigns.data, busStops.data.data.stops, parkingMeters.data]
 } catch (e) {
-    console.error(e); // 💩
+    console.error(e);  
   }
  }
 
