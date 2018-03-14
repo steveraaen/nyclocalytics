cc6d472b58824f5918f4200823a34da4b1a6b9df
Steveraaen <sraaen@gmail.com>
2018-03-14 16:50:50 -0400

    rev geocoding set up, metrics being sent to db

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   AppState,
+  AsyncStorage,
   Dimensions,
   FlatList,
   StyleSheet,
@@ -15,6 +16,7 @@ import {
 import { StackNavigator } from 'react-navigation';
 import Details from './Details.js'
 import getAll from './helpers'
+import axios from 'axios'
 
 export default class HomeScreen extends Component {
     static navigationOptions = {
@@ -23,7 +25,7 @@ export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state={
-      input: "Madison Square Garden"
+      input: null
     }  
     /*this.getData = this.getData.bind(this)*/
 /*    this._handleAppStateChange = this._handleAppStateChange.bind(this)
@@ -35,9 +37,88 @@ export default class HomeScreen extends Component {
     this.setState({
       appState: nextAppState,
     });*/
+/*    this.setMeta = this.setMeta.bind(this)*/
+    this.handlePress = this.handlePress.bind(this)
+/*    this.postMeta = this.postMeta.bind(this)*/
+    this.getPlaces = this.getPlaces.bind(this)
+    this.autoC = this.autoC.bind(this)
+    this.handlePlacePress = this.handlePlacePress.bind(this)
   }
+      handlePlacePress(id) {      
+      return axios.get('https://maps.googleapis.com/maps/api/place/details/json?placeid='+id+'&key=AIzaSyD0Zrt4a_yUyZEGZBxGULidgIWK05qYeqs', {
+    /*  return axios.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyD0Zrt4a_yUyZEGZBxGULidgIWK05qYeqs', {*/
+      }).then((respon) => {
+        this.setState({
+          details: respon,
+          uLatitude: respon.data.result.geometry.location.lat,
+          uLongitude: respon.data.result.geometry.location.lng,
+          address: respon.data.result.formatted_address.split(",")[0] + ", " + respon.data.result.formatted_address.split(",")[1],
+          uPlaceId: respon.data.result.place_id,
 
+        }, () => {
+                       getAll(this.state.uLongitude, this.state.uLatitude).then((response) => {  
+             console.log(response)    
+              this.setState({
+                citibikeStops: response[0],
+                subwayStops: response[1],
+                parkingASP: response[2],
+                busStops: response[3],
+                parkingMeters: response[4],
+                metrics: {  
+                  properties: {
+                    address: response[5].results[0].formatted_address,         
+                    place_id: response[5].results[0].place_id,         
+                    bikeLength: response[0].length, 
+                    subwayLength: response[1].length,
+                    ASPLength: response[2].length,
+                    busLength: response[3].length,
+                    metersLength: response[4].length,
+                    bikeDist: response[0][0].distance.dist,
+                    subwayDist: response[1][0].distance.dist
+                  },
+                  geometry: {
+                    type: "Point",
+                    coordinates: [this.state.uLongitude, this.state.uLatitude]
+                  }
+                },
+                 input:null,
+                 autoResp: null                 
+              })             
+            })
+        })
+      })
+    }
+  autoC(inp) {
+    if(this.state.autoResp) {
+    return (
+      <View>
+        <FlatList 
+          scrollEventThrottle={1}       
+          data={inp} 
+          renderItem={({item}) =>       
+            <TouchableOpacity 
+              style={{height: 30}}
+              onPress={() => this.handlePlacePress(item.place_id)}      
+              >
+                <View style={{ height: 40}} >
+                   <Text numberOfLines={1}style={{fontSize: 16,fontWeight: 'bold', color: 'white'}} >{item.description.split(",")[0] + "," + item.description.split(",")[1] +  "," + item.description.split(",")[2]  }</Text>
+                </View>
+            </TouchableOpacity>}
+          keyExtractor={item => item.id}
+        />
+          <View style={{marginTop: 20}}> 
+            <Text style={{color: 'yellow', textAlign: 'center', fontSize: 14, fontWeight: 'bold', paddingTop: 8}}>Tap on a place to see the nearest subway stations and next trains.</Text>
+          </View>
+        </View>
+      )
+    }
+  }
  componentDidMount() {
+/*              AsyncStorage.getItem('bLength').then((value) => {
+              this.setState({
+                bLength: value
+              })
+            }) */
   /*     AppState.addEventListener('change', this._handleAppStateChange);*/
       navigator.geolocation.getCurrentPosition(function(pos) {
             var { longitude, latitude, accuracy, heading } = pos.coords
@@ -46,24 +127,65 @@ export default class HomeScreen extends Component {
                 uLatitude: pos.coords.latitude,
                 uLnglat: [pos.coords.longitude, pos.coords.latitude]
             }, () => {
-             getAll(this.state.longitude, this.state.latitude).then((response) => {      
+              
+             getAll(this.state.uLongitude, this.state.uLatitude).then((response) => {  
+             console.log(response)    
               this.setState({
                 citibikeStops: response[0],
                 subwayStops: response[1],
                 parkingASP: response[2],
                 busStops: response[3],
-                parkingMeters: response[4]
-              })
+                parkingMeters: response[4],
+                metrics: {  
+                  properties: {
+                    address: response[5].results[0].formatted_address,         
+                    place_id: response[5].results[0].place_id,         
+                    bikeLength: response[0].length, 
+                    subwayLength: response[1].length,
+                    ASPLength: response[2].length,
+                    busLength: response[3].length,
+                    metersLength: response[4].length,
+                    bikeDist: response[0][0].distance.dist,
+                    subwayDist: response[1][0].distance.dist
+                  },
+                  geometry: {
+                    type: "Point",
+                    coordinates: [this.state.uLongitude, this.state.uLatitude]
+                  }
+                }                 
+              })             
             })
-            })   
+            })
 
         }.bind(this))    
-console.log(this.state)
-
  }
+/*             setMeta(value)  {
+
+   }*/
+   getPlaces(place) {
+     return axios.get('https://maps.googleapis.com/maps/api/place/autocomplete/json?input=' + place + '&radius=50000&minLength=2&administrative_area_level_1=NewYork&key=AIzaSyD0Zrt4a_yUyZEGZBxGULidgIWK05qYeqs', {
+        }).then((resp) => {
+          this.setState({
+            autoResp: resp.data.predictions
+          })
+          console.log(resp)
+        }).catch(function(error) {
+       throw error
+    }); 
+  }
  handlePress() {
-  console.log(this.state.uLongitude)
+  console.log(this.state.metrics)
+  
   getAll(this.state.uLongitude, this.state.uLatitude)
+/*    AsyncStorage.setItem('bLength', this.state.bikeLength.toString());
+    AsyncStorage.setItem('sLength', this.state.subwayLength.toString());
+    AsyncStorage.setItem('ALength', this.state.ASPLength.toString());
+    AsyncStorage.setItem('buLength', this.state.busLength.toString());
+    AsyncStorage.setItem('mLength', this.state.metersLength.toString());
+    AsyncStorage.setItem('uLong', this.state.uLongitude.toString());
+    AsyncStorage.setItem('mLat', this.state.uLatitude.toString());*/
+    /*axios.post('http://127.0.0.1:5000/api/meta/', { metrics: this.state.metrics })*/
+    axios.post('https://subs-backend.herokuapp.com/api/meta/', { metrics: this.state.metrics })
  }
   render() {
     const { navigate } = this.props.navigation;
@@ -84,12 +206,17 @@ console.log(this.state)
           <View style = {styles.searchBarInput}>
             <TextInput 
               style={{textAlign: 'center', fontSize: 20, color: 'black', backgroundColor: 'white'}}
+              value={this.state.input}
               placeholder="Madison Square Garden"
+              onChangeText={(text) => this.setState({input: text}, (text) => {this.getPlaces(this.state.input)})}                 
+  
             />
         </View>
+         <View style={styles.autoPlaces}>{this.autoC(this.state.autoResp)}</View>
         </View>
+        <View><Text numberOfRows={1} style={{color: 'white'}}> {this.state.metrics.properties.address}</Text></View>
           <View style={styles.resultsContainer}>
-            <TouchableOpacity onPress={() => this.handlePress()} > 
+            <TouchableOpacity onPress={() => this.handlePress(this.state.metrics)} > 
               <View style={styles.card}>
              
                 <Text style={styles.cardTitle}>Subways</Text>
@@ -169,10 +296,10 @@ const styles = StyleSheet.create({
     },
 // -------------------------results parent
     resultsContainer: {
- /*     marginTop:12,
-      paddingTop:12,*/
+/*      marginTop:12,
+      paddingTop:12,
       flex: .8,
-      flexDirection: 'column',
+      flexDirection: 'column',*/
     /*  flexWrap: 'wrap',*/
       backgroundColor: 'black'
     },
@@ -199,7 +326,13 @@ const styles = StyleSheet.create({
     },
     innerCard: {
       alignItems: 'flex-start'
-    }
+    },
+    autoPlaces: {
+      flexGrow: .3,
+    /*  width: this.state.width,*/
+      marginTop:14,
+      marginLeft: 14,
+    },
 
 });
 const nyclocalytics = StackNavigator({
--- helpers.js
+++ helpers.js
@@ -2,38 +2,41 @@ var axios = require ('axios')
 
 module.exports = async function getAll(lng, lat) {
 	try {
-
+console.log('getall called')
+console.log(lng + "," + lat)
 	 const citibikePromise = axios('https://subs-backend.herokuapp.com/api/bikes/', {
             params: {
-                lng: -73.9839440,
-                lat: 40.6766600,                 
+                lng: lng,
+                lat: lat,                 
 /*                lng: parseFloat(lng).toFixed(6),
                 lat: parseFloat(lat).toFixed(6) */              
             }
         })
 	 const subwayPromise = axios('https://subs-backend.herokuapp.com/api/stops/', {
             params: {
-                lng: -73.9839440,
-                lat: 40.6766600,               
+                lng: lng,
+                lat: lat,               
             }
         })
 	 const aspPromise = axios.get('https://streetparker.herokuapp.com/mon', {
-            params: { coordinates: [-73.9839440, 40.6766600]                            
+            params: { coordinates: [lng, lat]                            
             }
          })
     const bussesPromise = axios('https://bustime.mta.info/api/where/stops-for-location.json?latSpan=0.01&lonSpan=0.01&key=d780a773-f505-402c-b6cb-4a9939fbe07a', {
             params: {
-                lon: -73.9839440,
-                lat: 40.6766600,             
+                lon: lng,
+                lat: lat,             
             }    	
     })    
     const metersPromise =  axios('https://streetparker.herokuapp.com/api/meters', {
-            params: { coordinates: [-73.9839440, 40.6766600]                            
+            params: { coordinates: [lng, lat]                            
             }  	    	
     })
+    const revGeoPromise = axios('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat +',' + lng + '&key=AIzaSyD0Zrt4a_yUyZEGZBxGULidgIWK05qYeqs', {
+        })
 
-    const [citibikeStops, subwayStops, aspSigns, busStops, parkingMeters] = await Promise.all([citibikePromise, subwayPromise, aspPromise, bussesPromise, metersPromise]);
-    return [citibikeStops.data, subwayStops.data, aspSigns.data, busStops.data.data.stops, parkingMeters.data]
+    const [citibikeStops, subwayStops, aspSigns, busStops, parkingMeters, revGeo ] = await Promise.all([citibikePromise, subwayPromise, aspPromise, bussesPromise, metersPromise, revGeoPromise]);
+    return [citibikeStops.data, subwayStops.data, aspSigns.data, busStops.data.data.stops, parkingMeters.data, revGeo.data]
 } catch (e) {
     console.error(e);  
   }
